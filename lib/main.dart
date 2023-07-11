f1032cba8c5a820e1fdd2c23017d58da4148f069
haim1121 <haim112177@naver.com>
2023-07-11 22:12:03 +0900

    0711_haim_(1)_MainButtonClassSep

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,27 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text(
+              
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      MainButton(title: "여행 성향\n테스트"),
+                      MainButton(title: "여행지\n키워드 분석"),
+                    ],
+                  ),
+
+                  Row(
+                    children: [
+                      MainButton(title: "여행지\n추천 서비스"),
+                      MainButton(title: "여행일정\n추천 서비스"),
+                    ],
+                  ),
+                ],
+              ),
+
+
+              Text(
                 'You have pushed the button this many times:',
               ),
               Obx(
@@ -57,3 +77,46 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// MainPage에 있는 버튼
+class MainButton extends StatelessWidget {
+  MainButton({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextButton(
+        onPressed: (){
+
+        },
+        child: Container(
+          // 반응형으로 바꾸기
+          width: 200,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(Radius.circular(15))
+            //borderRadius: BorderRadius.circular(4),
+          ),
+          child:  Text(title,
+            style: TextStyle(fontSize: 15, fontFamily: 'SpoqaHanSansNeo',  color: Colors.black, ), textAlign: TextAlign.left, ),
+        ),
+
+        // text button의 default padding값을 없애주는 코드
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+        ),
+      ),
+    );
+  }
+}
+
+
